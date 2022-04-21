d5d50bf08eeb8abbb15070cf71ee77f65cf366ec
Ombeni Tango <ombenitango42@gmail.com>
2022-04-21 20:23:06 +0300

    Changing twig templating and updating composer.js file

--- Core/View.php
+++ Core/View.php
@@ -8,26 +8,6 @@ class View {
      * @param string $view  The view file
      * @param array $args  Associative array of data to
 
-    display in the view (optional)
-    *
-    * @return void
-    */
-   public static function render($view, $args = [])
-   {
-       extract($args, EXTR_SKIP);
-
-       $file = "../App/Views/$view";  // relative to Core directory
-
-       if (is_readable($file)) {
-           require $file;
-       } else {
-           //echo "$file not found";
-           throw new \Exception("$file not found");
-       }
-   }
-
-
-
     /**
      * Render a view file
      * @param string $view the view file
@@ -40,10 +20,16 @@ class View {
         static $twig = null;
 
         if ($twig === null) {
-            $loader = new \Twig_Loader_Filesystem('../App/Views');
-            $twig = new \Twig_Environment($loader);
+
+
+            $loader = new \Twig\Loader\FilesystemLoader('../App/Views');
+
+            $twig = new \Twig\Environment($loader, [
+                
+            ]);
         }
 
+       
         echo $twig->render($template, $args);
     }
   
